7c9cc17e42d6a175b1b57b5bda748673bf170884
devid <nnick4673@gmail.com>
2022-10-09 14:17:19 -0300

    Puxando as atividades que eu tenho que fazer no dia de hoje de dois bancos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from datetime import date as dt
 from dotenv import load_dotenv
 from os import getenv
 
@@ -7,6 +8,7 @@ load_dotenv()
 token = getenv('SECRET_API_KEY')
 
 def get_daily_tasks(db, name_db='não informado'):
+	today = str(dt.today())
 	url = f'https://api.notion.com/v1/databases/{db}/query'
 	headers = {
 		"Authorization": f"Bearer {token}",
@@ -16,11 +18,18 @@ def get_daily_tasks(db, name_db='não informado'):
 	r = requests.post(url, headers=headers)
 
 	if r.status_code == 200:
-		data = r.json()
-		tasks = data['results']
-		print(tasks)
+		dataset = r.json()
+		tasks = dataset['results']
+
+		for task in tasks:
+			name = task['properties']['Nome']['title'][0]['plain_text']
+			date = task['properties']['Data']['date']['start']
+
+			if date == today:
+				print(name)
 	else:
-		print(f'A conexão com o banco {name_db} não foi funcionou')
+		print(f'A conexão com o banco {name_db} não funcionou')
 
 if __name__ == '__main__':
 	get_daily_tasks(getenv('DATABASE_FRONT'), 'front')
+	get_daily_tasks(getenv('DATABASE_BACK'), 'back')
